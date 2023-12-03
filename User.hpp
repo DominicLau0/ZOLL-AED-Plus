03b7197d86984b66a4d72cbe3905be58abd0a73a
DottyV <147417915+DottyV@users.noreply.github.com>
2023-12-03 02:33:05 -0500

    Update User.h

--- User.hpp
+++ User.hpp
@@ -1,13 +1,21 @@
 //
-//  User.hpp
+//  User.h
 //  
 //
 //  Created by Dami on 02/12/2023.
 //
 
-#ifndef User_hpp
-#define User_hpp
-
+#ifndef USER_H
+#define USER_H
+#include <string>
 #include <stdio.h>
 
-#endif /* User_hpp */
+class User {
+public:
+    void performCPR();
+    void applyPads();
+private:
+    string userID;
+};
+
+#endif // USER_H
