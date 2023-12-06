6ee44288f70d314ca3fc9ffff975fe56ff05a59b
Twistedwater <70006848+Twistedwater@users.noreply.github.com>
2023-12-06 14:19:47 -0500

    Update AEDSimulator.cpp

--- AEDSimulator.cpp
+++ AEDSimulator.cpp
@@ -57,7 +57,7 @@ void AEDSimulator::analyzeHeartRhythm(Patient& patient) {
 
 void AEDSimulator::prepareForShock(Patient& patient,User& user) {
         //should depend on shockCount to adjust shockStrength
-    user.applypads();
+    user.applyPads();
     if (patient.getShockCount() == 0) {
         patient.setShockStrength(200); // Joules, anywhere between 120 and 200 Joules
         patient.setShockCount(1);
@@ -67,7 +67,7 @@ void AEDSimulator::prepareForShock(Patient& patient,User& user) {
     } else if (patient.getShockCount() > 0 && patient.getShockCount() <= 3) {
         patient.setShockStrength(360); // Joules, anywhere between 150 and 360 Joules for every shock after the first
         patient.setShockCount(patient.getShockCount() + 1);
-        deliverShock(patient)
+        deliverShock(patient);
     } else if (patient.getShockCount() > 3) {
         // After 3 shocks, call CPR and update heart rhythm
         user.performCPR();
