81b7cabff24cc92b309073bf59a0fc34eef4525e
Twistedwater <70006848+Twistedwater@users.noreply.github.com>
2023-12-06 22:53:01 -0500

    Update User.h

--- User.h
+++ User.h
@@ -23,7 +23,7 @@ public:
     void applyPads();
     int getCompressionStrength();
     int setCompressionStrength(int strength);
-    void generateCPRInstructions(User& user) const;
+    void generateCPRInstructions(Patient& patient) const;
 private:
     std::string userID;
     int compressionStrength;
