cf3920f443c4ccb43c79130d1efb4b0043fee8bd
Twistedwater <70006848+Twistedwater@users.noreply.github.com>
2023-12-06 19:22:09 -0500

    Add files via upload

---
+++ test_simulation.cpp
@@ -0,0 +1,34 @@
+// test_simulation.cpp
+#include <iostream>
+#include "User.h"
+#include "AEDSimulator.h"
+
+// Simple assert function for testing
+void assert(bool condition, const char* message) {
+    if (!condition) {
+        std::cerr << "Test failed: " << message << std::endl;
+    } else {
+        std::cout << "Test passed: " << message << std::endl;
+    }
+}
+
+int main() {
+    // Create dummy objects for testing
+    Patient testPatient;
+    User testUser;
+    AEDSimulator testAEDSimulator;
+
+    // Test User's functions
+    testUser.performCPR(testPatient);
+    assert(/* Add a condition based on your User.cpp logic */, "performCPR test");
+    testUser.applyPads();
+    assert(/* Add a condition based on your User.cpp logic */, "applyPads test");
+
+    // Test AEDSimulator's function
+    testAEDSimulator.analyzeHeartRhythm(testPatient);
+    assert(/* Add a condition based on your AEDSimulator.cpp logic */, "analyzeHeartRhythm test");
+
+    // Add more test cases as needed
+
+    return 0;
+}
